f2b622111c975ce7be6dfe47de5c4c95daa90308
jval1972 <jvalavanis@gmail.com>
2021-01-25 05:02:17 +0200

    Update year in copyright notice

--- p_ceilng.pas
+++ p_ceilng.pas
@@ -3,7 +3,7 @@
 //  FPCDoom - Port of Doom to Free Pascal Compiler
 //  Copyright (C) 1993-1996 by id Software, Inc.
 //  Copyright (C) 2004-2007 by Jim Valavanis
-//  Copyright (C) 2017-2018 by Jim Valavanis
+//  Copyright (C) 2017-2021 by Jim Valavanis
 //
 //  This program is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License
@@ -113,7 +113,6 @@ end;
 //
 // T_MoveCeiling
 //
-
 procedure T_MoveCeiling(ceiling: Pceiling_t);
 var
   res: result_e;
